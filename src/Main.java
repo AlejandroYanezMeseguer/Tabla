cdb48c7234596e26016b905bbf7e18d404996101
AlejandroYanezMeseguer <ayanezmeseguer@danielcastelao.org>
2024-04-24 11:26:46 +0200

    terminado

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[]args){
+
+    Tabla miTabla = new Tabla();
+    miTabla.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+}
